b649d86dcf27e98c02a5ee65151c29c06946fd28
nilphym <nilphym@gmail.com>
2021-07-23 12:06:24 +0200

    Add fake account for testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import database from './utils/database';
+import { USERS_PROFILES } from './utils/constants';
 import SiteHeading from './containers/SiteHeading';
 import MarketPage from './containers/MarketPage';
 import { wrapper } from './App.module.scss';
 
-const App = () => (
-  <div className={wrapper}>
-    <SiteHeading />
-    <MarketPage />
-  </div>
-);
+const App = () => {
+  database.setItem(USERS_PROFILES, [{
+    userId: 1,
+    funds: {
+      ETH: 5,
+      USD: 10
+    }
+  }]);
+  return (
+    <div className={wrapper}>
+      <SiteHeading />
+      <MarketPage />
+    </div>
+  );
+};
 
 export default App;
