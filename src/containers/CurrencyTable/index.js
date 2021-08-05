c370c37f5a9921d7c170e1937c9e1150bf509aa6
nilphym <nilphym@gmail.com>
2021-08-05 16:03:39 +0200

    Show last currency table view on error or loading

--- src/containers/CurrencyTable/index.js
+++ src/containers/CurrencyTable/index.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CurrencyTable from './CurrencyTable';
-import Text from '../../components/Text';
 
 const useStyles = makeStyles(() => ({
   center: {
@@ -13,6 +12,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 let canShowSpinner;
+let stockDataPrev = [];
 
 const CurrencyTableWrapper = () => {
   const { isLoading, isError, stockData } = useSelector((state) => state.stock);
@@ -27,18 +27,11 @@ const CurrencyTableWrapper = () => {
     return <div className={classes.center}><CircularProgress /></div>;
   }
 
-  if (isError) {
-    return (
-      <div className={classes.center}>
-        <Text
-          text="There was error when connecting to API. Please wait a couple of seconds and refresh the page."
-          type="HEADING_4"
-          state="ERROR"
-        />
-      </div>
-    );
+  if (isError || isLoading) {
+    return <CurrencyTable stockExchangeData={stockDataPrev} />;
   }
 
+  stockDataPrev = stockData;
   return <CurrencyTable stockExchangeData={stockData} />;
 };
 
